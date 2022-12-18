d91f95a3c84baad0677dd137e7bd23aaaab15214
devLucasMoraes <devlucasmoraes@gmail.com>
2022-12-18 13:16:35 -0300

    initial commit

---
+++ src/test/java/med/voll/apiSpringBoot/ApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package med.voll.apiSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
