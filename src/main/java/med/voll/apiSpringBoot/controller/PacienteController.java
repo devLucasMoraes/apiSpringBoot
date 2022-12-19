08ab2de0294936ae6593d56d24aa4c018d258912
devLucasMoraes <devlucasmoraes@gmail.com>
2022-12-18 22:30:09 -0300

    @PostMapping - persistencia de Paciente

--- src/main/java/med/voll/apiSpringBoot/controller/PacienteController.java
+++ src/main/java/med/voll/apiSpringBoot/controller/PacienteController.java
@@ -1,6 +1,10 @@
 package med.voll.apiSpringBoot.controller;
 
+import jakarta.validation.Valid;
 import med.voll.apiSpringBoot.paciente.CadastroPacienteDTO;
+import med.voll.apiSpringBoot.paciente.Paciente;
+import med.voll.apiSpringBoot.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pacientes")
 public class PacienteController {
 
+    @Autowired
+    PacienteRepository repository;
     @PostMapping
-    public void cadastrar(@RequestBody CadastroPacienteDTO dados){
-        System.out.println(dados);
+    public void cadastrar(@RequestBody @Valid CadastroPacienteDTO dados){
+        repository.save(new Paciente(dados));
     }
 }
--- src/main/java/med/voll/apiSpringBoot/paciente/CadastroPacienteDTO.java
+++ src/main/java/med/voll/apiSpringBoot/paciente/CadastroPacienteDTO.java
@@ -1,6 +1,24 @@
 package med.voll.apiSpringBoot.paciente;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import med.voll.apiSpringBoot.endereco.DadosEndereco;
 
-public record CadastroPacienteDTO(String nome, String email, String telefone, String cpf, DadosEndereco dadosEndereco) {
+public record CadastroPacienteDTO(
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+
+        String telefone,
+        @NotBlank
+
+        String cpf,
+        @NotNull @Valid
+        DadosEndereco dadosEndereco
+) {
 }
